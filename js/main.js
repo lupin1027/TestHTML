35b5e2d401d4d6d9e3e8127eae990d1411243301
t <hoge@hoge.com>
2024-09-09 23:35:23 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+﻿var data;
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    data = readJson('./data.json');
+
+    if(data == null){
+    	console.log('data読込失敗');
+    }
+    else{
+	console.log('data読込成功');
+    }
+
+    console.log(data);
+});
+
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    txtValue = li[i].textContent || li[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
---
+++ js/util.js
@@ -0,0 +1,23 @@
+﻿function readJson(filePath) {
+
+  console.log(filePath);
+  
+  $.get(filePath, function(data) {
+    var data = $.parseJson(data);
+    return data;
+  });
+}
+
+function isExistFile(file) {
+   try {
+     const fs = require('fs');
+     fs.statSync(file);
+     console.log('true');
+     return true;
+   } catch (err) {
+     if (err.code === 'ENOENT') {
+       console.log('false');
+       return false;
+     }
+   }
+}
