5d0cec47ac2c10ce6a9c603f33a403ad396d12ce
t <hoge@hoge.com>
2024-09-10 00:11:27 +0900

    fix

--- js/util.js
+++ js/util.js
@@ -2,9 +2,24 @@
 
   console.log(filePath);
   
+  $.ajax({
+  	// ajax読み込みの設定
+  	type: "GET",
+  	url: "data.json", // jsonまでのファイルパス
+ 	dataType: "json", // ファイル形式
+ 	async: false, // 非同期通信フラグ
+  	}).then(
+  	function (json) {
+
+	});
+
+  
+  /*
   $.getJSON(filePath, function(data) {
     return data;
   });
+  */
+
 }
 
 function isExistFile(file) {
